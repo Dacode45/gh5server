69b0763b14f4d0389656856490be766ba2c1ebb2
David Ayeke <ayekedavid@yahoo.com>
2015-09-12 23:42:46 -0500

    Added recover function for handlers

--- logger.go
+++ logger.go
@@ -4,12 +4,28 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"errors"
 )
 
 func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
+		var err error
+			defer func() {
+					r := recover()
+					if r != nil {
+							switch t := r.(type) {
+							case string:
+									err = errors.New(t)
+							case error:
+									err = t
+							default:
+									err = errors.New("Unknown error")
+							}
+							http.Error(w, err.Error(), http.StatusInternalServerError)
+					}
+			}()
 		inner.ServeHTTP(w, r)
 
 		log.Printf(
